21bb306cf1d25c11d14016b41b985bd96bb40948
Andrei Papruha <andreipopruha@gmail.com>
2022-11-09 15:27:26 +0400

    change variable 10 to 15

--- src/main/java/lap.java
+++ src/main/java/lap.java
@@ -1,6 +1,6 @@
 public class lap {
     public static void main(String[] args) {
-        int n = 10;
+        int n = 15;
         for (int i = 1; i <= n; ++i) {
             System.out.println("Java is fun");
 
